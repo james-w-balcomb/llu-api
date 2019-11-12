208de2ee052b33e3923777010d0325bea6f08681
james-w-balcomb <james.w.balcomb@gmail.com>
2019-11-12 10:39:59 -0500

    add deployment scripts

---
+++ deploy_scp_commands.sh
@@ -0,0 +1,16 @@
+scp /c/Development/LLU-API/llu-api/.env.development jbalcomb@172.81.134.163:/opt/llu-api/
+scp /c/Development/LLU-API/llu-api/.env.production jbalcomb@172.81.134.163:/opt/llu-api/
+scp /c/Development/LLU-API/llu-api/.gitignore jbalcomb@172.81.134.163:/opt/llu-api/
+scp /c/Development/LLU-API/llu-api/app.js jbalcomb@172.81.134.163:/opt/llu-api/
+scp /c/Development/LLU-API/llu-api/controllers.js jbalcomb@172.81.134.163:/opt/llu-api/
+scp /c/Development/LLU-API/llu-api/deploy.sh jbalcomb@172.81.134.163:/opt/llu-api/
+scp /c/Development/LLU-API/llu-api/deploy_scp_commands.sh jbalcomb@172.81.134.163:/opt/llu-api/
+scp /c/Development/LLU-API/llu-api/models.js jbalcomb@172.81.134.163:/opt/llu-api/
+scp /c/Development/LLU-API/llu-api/package-lock.json jbalcomb@172.81.134.163:/opt/llu-api/
+scp /c/Development/LLU-API/llu-api/package.json jbalcomb@172.81.134.163:/opt/llu-api/
+scp /c/Development/LLU-API/llu-api/README.md jbalcomb@172.81.134.163:/opt/llu-api/
+scp /c/Development/LLU-API/llu-api/routes.js jbalcomb@172.81.134.163:/opt/llu-api/
+scp /c/Development/LLU-API/llu-api/schema.js jbalcomb@172.81.134.163:/opt/llu-api/
+# scp -r /c/Development/LLU-API/llu-api/.git jbalcomb@172.81.134.163:/opt/llu-api/
+# scp -r /c/Development/LLU-API/llu-api/.idea jbalcomb@172.81.134.163:/opt/llu-api/
+# scp -r /c/Development/LLU-API/llu-api/node_modules jbalcomb@172.81.134.163:/opt/llu-api/
